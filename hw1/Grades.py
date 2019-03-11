7d7f837bc65c6eb1a9d50d18a75eae3a24ee5ef2
Niv Tik <nivtik@gmail.com>
2019-03-11 18:22:37 +0200

    updates

--- hw1/Grades.py
+++ hw1/Grades.py
@@ -1,6 +1,4 @@
-History={'Subject': 'History','James': [100,93], 'Monica': [99,97], 'Reggie': [73,99], 'Jenn': [54,91], 'Albert': [44,100]}
-Math={'Subject': 'Math','James': [55,91], 'Monica': [77,66], 'Reggie': [86,92],'Mike':[33,99], 'Jenn': [64,98],}
-Result={}
+
 def compare_subjects_within_student(subj1,subj2):
     for name in subj1:
         if type(subj1[name]) is list:
@@ -15,6 +13,10 @@ def compare_subjects_within_student(subj1,subj2):
                     Result[name]= subj2['Subject']
     return Result
 
-Result=compare_subjects_within_student(History,Math)
-print(Result)
+if __name__ == '__main__':
+    subj1={'Subject': 'History','James': [100,93], 'Monica': [99,97], 'Reggie': [73,99], 'Jenn': [54,91], 'Albert': [44,100]}
+    subj2={'Subject': 'Math','James': [55,91], 'Monica': [77,66], 'Reggie': [86,92],'Mike':[33,99], 'Jenn': [64,98],}
+    Result={}
+    solution=compare_subjects_within_student(subj1,subj2)
+    print(f"Best grades: {solution}")
 
