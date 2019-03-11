98580faaf73edd608de71ba9be0dab05ba3fd86b
Niv Tik <nivtik@gmail.com>
2019-03-11 18:34:35 +0200

    update

--- hw1/trifeca.py
+++ hw1/trifeca.py
@@ -4,8 +4,13 @@ def trifeca(word):
         return False
     for i in range(len(word)-5):
         if word[i]==word[i+1] and word[i+2]==word[i+3] and word[i+4]==word[i+5]:
+                print(f'Word contains 3 consecutive double letters')
                 return True
+    
+    print(f'Word does not contain 3 consecutive double letters')   
     return False
-
-answer=trifeca('aabbcc')
-print(answer)
+    
+if __name__ == '__main__':
+    word='aabbcc'
+    trifeca(word)
+   
